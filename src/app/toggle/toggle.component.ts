2b411ed81fb349db98ba5ca70ff71072d6ef300f
taiphanvan2k3 <taiphanvan2403@gmail.com>
2023-11-02 20:53:35 +0700

    Lesson9: Two ways data binding

---
+++ src/app/toggle/toggle.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle',
+  templateUrl: './toggle.component.html',
+  styleUrls: ['./toggle.component.css']
+})
+export class ToggleComponent {
+  @Input() checked!: boolean;
+  @Output() checkedChange = new EventEmitter<boolean>();
+
+  toggle() {
+    this.checked = !this.checked;
+    this.checkedChange.emit(this.checked);
+  }
+}
